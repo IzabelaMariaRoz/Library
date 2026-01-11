528e6d82e2c24e1696cdc89dda927288c94fa7c2
izabelarozalska <izarozalska2003@gmail.com>
2026-01-11 19:46:35 +0100

    Add and update code in book.hpp

--- include/Book.hpp
+++ include/Book.hpp
@@ -1,27 +1,51 @@
 #pragma once
 #include <string>
+#include <sstream>
+using namespace std;
 
 class Book {
 private:
-    std::string title;
-    std::string author;
+    string title;
+    string author;
     int isbn;
     int yearOfPublishing;
-    bool isBorrowed;
+    int quantity;        //ile egze. biblioteka posiada
+    int borrowedCout;    //aktualnie wypożyczonych
 
 public:
-    Book();
-    Book(const std::string& title, const std::string& author, int isbn, int year);
+    // Konstruktor domyślny
+    Book() : title(""), author(""), isbn(0), yearOfPublishing(0), quantity(1), borrowedCount(0) {}
+    //konstruktor pełny
+    Book(const string& title, const string& author, int isbn, int year, int quantity = 1)
+        : title(title), author(author), isbn(isbn), yearOfPublishing(year), quantity(quantity), borrowedCount(0) {}
 
-    std::string getTitle() const;
-    std::string getAuthor() const;
-    int getISBN() const;
-    int getYear() const;
-    bool getBorrowedStatus() const;
 
-    void borrow();
-    void returnBook();
 
-    std::string serialize() const;
-    void deserialize(const std::string& data);
+      //getter
+    string getTitle() const{return title;}
+    string getAuthor() const{return author;}
+    int getISBN() const{return isbn;}
+    int getYear() const{return yearOfPublishing;}
+    int getQuantity() const{return quantity;}
+    int getBorrowedCount() const { return borrowedCount; }
+    int getAvailable() const{return quantity - borrowedCount;}
+
+
+    //wypożyczanie książki
+    bool borrow(){
+        if(borrowedCount<quantity){
+            borrowedCount++;
+            return true;
+        }
+        return false;
+    }
+
+    //zwracanie
+    void returnBook(){
+        if(borrowedCount>0) borrowedCount--;
+    }
+
+
+    string serialize() const;
+    void deserialize(const string& data);
 };
